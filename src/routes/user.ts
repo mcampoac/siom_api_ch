502ea05cd29c110850d24bb98cc62ea1fca6ef7f
Macdiel Campos <macdiel.campos27@gmail.com>
2021-04-30 17:45:51 -0400

    Version 1

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -9,14 +9,11 @@ router.get('/', UserController.getAll); //Metodo
 // Obtener un usuario - Get one user
 router.get('/', UserController.getById);
 
-
 // Crear un usuario - Create a new user
 //router.post('/', UserController.newUser);
 
-
 // Editar usuario - Edit user
 router.patch('/', UserController.editUser);
-
 
 // Eliminar un usuario - Delete user
 router.delete('/', UserController.deleteUser);

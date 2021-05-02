09c98de548683022f8208e1e71744c2990831aec
José Hernández <jhernanq@everis.com>
2021-05-02 14:56:35 -0400

    feature/fase-1-rest-api-ch
    mejora backend

--- src/controller/UsuarioController.ts
+++ src/controller/UsuarioController.ts
@@ -5,33 +5,34 @@ import { validate } from 'class-validator';
 
 export class UsuarioController {
 
-    static getAll = async (req: Request, res: Response) => {
-        const userRepository = getRepository(Usuario);
-      let usuario;
+  static getAll = async (req: Request, res: Response) => {
+    const userRepository = getRepository(Usuario);
+    let usuario;
 
-      try {
-        usuario = await userRepository.find({ select: ['usua_login', 'usua_password', 'usua_rut', 'usua_alias', 'usua_nombre'] });
-      } catch (e) {
-        res.status(404).json({ message: 'Somenthing goes wrong!' });
-      }
+    try {
+      usuario = await userRepository.find({ select: ['usua_login', 'usua_password', 'usua_rut', 'usua_alias', 'usua_nombre'] });
+    } catch (e) {
+      res.status(404).json({ message: 'Somenthing goes wrong!' });
+    }
 
-      if (usuario.length > 0) {
-        res.send(usuario);
-      } else {
-        res.status(404).json({ message: 'Not result' });
-      }
-    };
+    if (usuario.length > 0) {
+      res.send(usuario);
+    } else {
+      res.status(404).json({ message: 'Not result' });
+    }
+  };
 
-    static getById = async (req: Request, res: Response) => {
-      const { id } = req.params;
-      const userRepository = getRepository(Usuario);
-      try {
-        const user = await userRepository.findOneOrFail(id);
-        res.send(user);
-      } catch (e) {
-        res.status(404).json({ message: 'Not result' });
-      }
-    };
+  static getById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    let usua_id = +id;
+    const userRepository = getRepository(Usuario);
+    try {
+      const user = await userRepository.findOne({ select: ['usua_login', 'usua_password', 'usua_rut', 'usua_alias', 'usua_nombre'], where: { usua_id: usua_id } });
+      res.send(user);
+    } catch (e) {
+      res.status(404).json({ message: 'Not result' });
+    }
+  };
 }
 
 export default UsuarioController;
